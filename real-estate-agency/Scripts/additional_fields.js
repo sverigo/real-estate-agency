f7c20d73b10f8f16774758839f58b261d00e73df
Stanislav <kw74sv@gmail.com>
2018-05-24 18:35:24 +0300

    add/edit customization: v1.00

--- real-estate-agency/Scripts/additional_fields.js
+++ real-estate-agency/Scripts/additional_fields.js
@@ -1,19 +1,19 @@
 ﻿var j = 1;
 function add_phone_field() {
     var name = "Phone" + j;
-    var button = '<input type="button" value="Х" id="delete' + j + '" onclick="$(\'#' + name + '\').remove(); $(\'#delete' + j+'\').remove();" />';
-    var input = '<input type="tel" name="' + name + '" id="' + name +'"/>';
+    var button = '<input class="form-control" type="button" value="Х" id="delete' + j + '" onclick="$(\'#' + name + '\').remove(); $(\'#delete' + j+'\').remove();" />';
+    var input = '<input class="form-control" type="tel" name="' + name + '" id="' + name +'"/>';
 
-    $("#phonediv").append('<div class="but">' + button + input + '</div>');
+    $("#phonediv").append('<div class="but form-group">' + button + input + '</div>');
     j++;
 }
 
 var i = 1;
 function add_photo_field() {
     var name = "Images" + i;
-    var input_file = '<input type="file" name="' + name + '" id="' + name + '" />';
-    var input_button = '<input type="button" value="Х" id="remove' + i + '" onclick="$(\'#' + name + '\').remove(); $(\'#remove' + i + '\').remove();" />';
+    var input_file = '<input class="form-control" type="file" name="' + name + '" id="' + name + '" />';
+    var input_button = '<input class="form-control" type="button" value="Х" id="remove' + i + '" onclick="$(\'#' + name + '\').remove(); $(\'#remove' + i + '\').remove();" />';
 
-    $("#photodiv").append('<div class="but">' + input_button + input_file + '</div>');
+    $("#photodiv").append('<div class="but form-group">' + input_button + input_file + '</div>');
     i++;
 }
