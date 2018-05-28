a9b2e87fcea047f14967fed29049ee16657bd8f1
Stanislav <kw74sv@gmail.com>
2018-05-28 20:05:05 +0300

    fix fancybox with google drive images

--- real-estate-agency/Scripts/gallery-group.js
+++ real-estate-agency/Scripts/gallery-group.js
@@ -1,5 +1,11 @@
 ﻿$(document).ready(function () {
     $("a.group-item").fancybox({
+        'type': 'image',
+        'maxWidth': 800,
+        'maxHeight': 600,
+        'width': '70%',
+        'height': '70%',
+        'autoSize': false,
         'transitionIn': 'elastic',
         'transitionOut': 'elastic',
         'speedIn': 600,
